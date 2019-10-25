ef7a3555f5ee70e74292755856d783ed124b8bdc
fedeportillo.unla@gmail.com <fedeportillo.unla@gmail.com>
2019-10-25 05:40:56 +0000

    publications

--- src/app/pages/tab2/tab2.module.ts
+++ src/app/pages/tab2/tab2.module.ts
@@ -4,27 +4,21 @@ import { NgModule, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab2Page } from './tab2.page';
+import { PublicationsComponent } from '../../components/publications/publications.component';
 import { PublicationsService } from '../../services/publications.service';
-import { PetPublication } from '../../interfaces/interfaces';
 
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
+    PublicationsComponent,
     RouterModule.forChild([{ path: '', component: Tab2Page }])
+  ],
+  providers: [
+    PublicationsService
   ],
   declarations: [Tab2Page]
 })
-export class Tab2PageModule implements OnInit {
-
-  publications: PetPublication[] = [];
-
-  constructor(private publicationService: PublicationsService) {
-
-  }
-
-  ngOnInit() {
-    this.publications = this.publicationService.getPetPublications();
-  }
+export class Tab2PageModule {
 }
--- src/app/pages/tab2/tab2.page.ts
+++ src/app/pages/tab2/tab2.page.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PublicationsService } from '../../services/publications.service';
+import { PetPublication } from 'src/app/interfaces/interfaces';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit {
 
-  constructor() {}
+  publications: PetPublication[] = [];
 
+  constructor(private publicationService: PublicationsService) {
+
+  }
+
+  ngOnInit() {
+    this.publications = this.publicationService.getPetPublications();
+  }
 }
